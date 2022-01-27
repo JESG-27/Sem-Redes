b0b1020d6d2bb9bbbc852f48d3fce342f8df3334
JESG-27 <jose.salcido4178@alumnos.udg.mx>
2022-01-27 11:39:38 -0600

    Modificación formato visual

--- Ethernet.h
+++ Ethernet.h
@@ -3,56 +3,79 @@
 
 #include <iostream>
 #include <stdio.h>
+//#include "ArregloDinamico.h"
 
 using namespace std;
 
 void ethernet(FILE *archivo)
 {
-    
-    unsigned char palabra;
+    unsigned char palabra[70];
+    //ArregloDinamico<unsigned char> palabra;
     size_t i;
+    
+    cout << "<<< ETHERNHET >>>" << endl << endl;
 
-        cout << "Direccion MAC origen:" << endl;
-        for(i=0;i<=5;i++)
-        {
-            palabra=fgetc(archivo);
-            printf ("%02x:",palabra);
-        }
-        cout << endl << endl;
+    cout << "Direccion MAC Destino:" << endl;
+    for(i=0;i<=5;i++)
+    {
+        palabra[i]=fgetc(archivo);
+        printf ("%02x:",palabra[i]);
+    }
+    cout << endl << endl;
 
-        cout << "Direccion MAC destino:" << endl;
-        for(i=0;i<=5;i++)
-        {
-            palabra=fgetc(archivo);
-            printf ("%02x:",palabra);
-        }
-        cout << endl << endl;
+    cout << "Direccion MAC Origen:" << endl;
+    for(i=6;i<=11;i++)
+    {
+        palabra[i]=fgetc(archivo);
+        printf ("%02x:",palabra[i]);
+    }
+    cout << endl << endl;
 
 
-        cout << "Tipo de codigo:" << endl;
-        for(i=0;i<=1;i++)
-        {
-            palabra=fgetc(archivo);
-            printf ("%02x:",palabra);
-            printf ("%c", palabra);
+    cout << "Tipo de codigo:" << endl;
+    for(i=12;i<=13;i++)
+    {
+        palabra[i]=fgetc(archivo);
+        printf ("%02x:",palabra[i]);
+    }
 
-            // switch con las opciones de los diferentes tipos
-            // 0800 IPv4
-            // 0806 ARP
-            // 8035 RARP
-            // 86DD IPv6
-   
-        }
-        cout << endl << endl;
+    // 0800 IPv4
+    // 0806 ARP
+    // 8035 RARP
+    // 86DD IPv6
+    
+    cout << endl;
+    if (palabra[13] == 0)
+    {
+        cout << "IPv4" << endl;
+    }
 
-        cout << "Datos: " << endl;
-        while (!feof(archivo))
-        {
-            palabra=fgetc(archivo);
-            printf ("%02x:",palabra);
-        }
-        cout << endl << endl;
+    else if (palabra[13] == 6)
+    {
+        cout << "ARP" << endl;
+    }
 
+    else if (palabra[13] == 5)
+    {
+        cout << "RARP" << endl;
+    }
+
+    else if (palabra[13] == 'D')
+    {
+        cout << "IPv6" << endl;
+    }
+
+    cout << endl;
+
+    cout << "Datos: " << endl;
+
+    while (!feof(archivo))
+    {
+        i++;
+        palabra[i]=fgetc(archivo);
+        printf ("%02x:",palabra[i]);
+    }
+    cout << endl << endl;
     fclose(archivo);
 }
 

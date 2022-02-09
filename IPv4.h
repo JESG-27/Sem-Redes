111c452bf152fdafd32c9248a3144a091b4b16e3
JESG-27 <jose.salcido4178@alumnos.udg.mx>
2022-02-08 20:00:05 -0600

    Actualización

--- IPv4.h
+++ IPv4.h
@@ -18,12 +18,14 @@ void IPv4(FILE *archivo)
         i++;
     }
     cont=i;
-    
-    //////////////////////////////////////////////////////// VERSION //  Primer Byte primeros 4 bits 
+
+    //////////////////////////////////////////////////////// VERSION //  Primer Byte primeros 4 bits
     // IPv4 0100
     // IPv6 0110
 
     conv = ASCII_BIN(datos[0]);
+    
+    cout << "Version: ";
     if (conv[2]==0)
     {
         for (i=0; i<4; i++)
@@ -43,16 +45,59 @@ void IPv4(FILE *archivo)
     }
 
     ///////////////////////////////////////////////////// Tamaño /////
-    
+    cout << "Tamano: ";
+    for (i=4; i<=7; i++)
+    {
+        cout << conv[i];
+    }
+    int tam = ASCII_DEC(datos[0], 4);
+    cout << dec << " " << tam;
+    cout << endl;
+
     ///////////////////////////////////////////////////// Tipo de Servicio ///
     conv = ASCII_BIN(datos[1]);
 
     cout << "Desglose de Bits: ";
-    if (conv[0]==0 && conv[1]==0 && conv[2]==0)
+    if (conv[0]==0 && conv[1]==0 && conv[2]==0) //000
     {
         cout << "De rutina" << endl;
     }
-    // Agregar demás casos de 4 bits
+
+    else if (conv[0]==0 && conv[1]==0 && conv[2]==1) //001
+    {
+        cout << "Prioritario" << endl;
+    }
+
+    else if (conv[0]==0 && conv[1]==1 && conv[2]==0) //010
+    {
+        cout << "Inmediato" << endl;
+    }
+
+    else if (conv[0]==0 && conv[1]==1 && conv[2]==1) //011
+    {
+        cout << "Relampago" << endl;
+    }
+
+    else if (conv[0]==1 && conv[1]==0 && conv[2]==0) // 100
+    {
+        cout << "Invalidacion Relampago" << endl;
+    }
+
+    else if (conv[0]==1 && conv[1]==0 && conv[2]==1) //101
+    {
+        cout << "Proceso de llamada critica y de emergencia" << endl;
+    }
+
+    else if (conv[0]==1 && conv[1]==1 && conv[2]==0) //110
+    {
+        cout << "Control de trabajo de internet" << endl;
+    }
+
+    else if (conv[0]==1 && conv[1]==1 && conv[2]==1) //111
+    {
+        cout << "Control de red" << endl;
+    }
+
 
     cout << "Retardo: ";
     if (conv[3]==0)
@@ -83,6 +128,7 @@ void IPv4(FILE *archivo)
     {
         cout << "Alta" << endl;
     }
+    cout << endl;
 
     //////////////////////////////////////////// Longitud Total //
     conv = ASCII_BIN(datos[2], datos[3]);
@@ -92,7 +138,80 @@ void IPv4(FILE *archivo)
     {
         cout << conv[i];
     }
-    
+    cout << " ";
+    int longitud = ASCII_DEC(datos[2], datos[3]);
+    cout << dec << longitud << endl;
+
+    ////////////////////////////////////////// Identificador ///
+    conv = ASCII_BIN(datos[4], datos[5]);
+
+    cout << "Identificador: ";
+    for (i=0; i<16; i++)
+    {
+        cout << conv[i];
+    }
+    cout << " ";
+    longitud = ASCII_DEC(datos[4], datos[5]);
+    cout << longitud << endl;
+
+    ////////////////////////////////////////////// Flags ///
+    conv = ASCII_BIN(datos[6]);
+
+    cout << "Flags: ";
+    for (i=0; i<=2; i++)
+    {
+        cout << conv[i];
+    }
+    cout << endl;
+
+    ////////////////////////// Posición de Fragmento //
+    conv = ASCII_BIN(datos[6], datos[7]);
+
+    cout << "Posicio de Fragmento: ";
+    for (i=3; i<16; i++)
+    {
+        cout << conv[i];
+    }
+    cout << endl;
+
+    ///////////////////////////////////////// Tiempo de Vida ///
+    conv = ASCII_BIN(datos[8]);
+
+    cout << "Tiempo de Vida: ";
+    for (i=0; i<8; i++)
+    {
+        cout << conv[i];
+    }
+    cout << " ";
+    int num = ASCII_DEC(datos[8]);
+    cout << dec << num;
+    cout << endl;
+
+    //////////////////////////////////////// Protocolo ////
+    conv = ASCII_BIN(datos[9]);
+
+    cout << "Protocolo: ";
+    for (i=0; i<8; i++)
+    {
+        cout << conv[i];
+    }
+    cout << endl;
+
+    /////////////////////////////////// Headchecksum  //////
+    conv = ASCII_BIN(datos[10], datos[11]);
+
+    cout << "Headchecksum: ";
+    for (i=0; i<16; i++)
+    {
+        cout << conv[i];
+    }
+    cout << endl;
+
+    ////////////////////////////////// IP Origen /////
+
+
+
+    ////////////////////////////////// IP Destino /////
 
 }
 

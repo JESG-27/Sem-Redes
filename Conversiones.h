02ba75559e7ace6c3249b1e6eb22ce1f0f16e4d9
JESG-27 <jose.salcido4178@alumnos.udg.mx>
2022-02-08 20:00:40 -0600

    Funciones Bin a Dec

--- Conversiones.h
+++ Conversiones.h
@@ -5,6 +5,7 @@
 
 using namespace std;
 
+int decimal=0;
 
 int *ASCII_BIN(unsigned char entrada)
 {
@@ -20,7 +21,6 @@ int *ASCII_BIN(unsigned char entrada)
 
     for(bottom=0,top =7; bottom<8; bottom++,top--)
     {
-        //cout<<m[top];
         conv[bottom] = m[top];
     }
 
@@ -58,14 +58,323 @@ int *ASCII_BIN(unsigned char entrada, unsigned char entrada2)
         prt2[bottom] = m[top];
         conv[bottom+8]=prt2[bottom];
     }
-    
+
     return conv;
 }
 
-int BIN_DEC(int num[])
+int ASCII_DEC(unsigned char entrada)
 {
-    
+    size_t i;
+    int m[8], conv[8], top, bottom, decimal=0;
+    //int conv[8];
+
+    for(i=0;i<8;i++)
+    {
+        m[i]=entrada%2;
+        entrada = entrada/2;
+    }
+
+    for(bottom=0,top =7; bottom<8; bottom++,top--)
+    {
+        conv[bottom] = m[top];
+    }
+
+    for (i=0;i<8;i++)
+    {
+        switch (i)
+        {
+        case 0:
+            if (conv[i]==1)
+            {
+                decimal=decimal+128;
+            }
+        break;
+
+        case 1:
+            if (conv[i]==1)
+            {
+                decimal=decimal+64;
+            }
+        break;
+
+        case 2:
+            if (conv[i]==1)
+            {
+                decimal=decimal+32;
+            }
+        break;
+
+        case 3:
+            if (conv[i]==1)
+            {
+                decimal=decimal+16;
+            }
+        break;
+
+        case 4:
+            if (conv[i]==1)
+            {
+                decimal=decimal+8;
+            }
+        break;
+
+        case 5:
+            if (conv[i]==1)
+            {
+                decimal=decimal+4;
+            }
+        break;
+
+        case 6:
+            if (conv[i]==1)
+            {
+                decimal=decimal+2;
+            }
+        break;
+
+        case 7:
+            if (conv[i]==1)
+            {
+                decimal=decimal+1;
+            }
+        break;
+
+        default:break;
+        }
+    }
+    //cout << dec << decimal << endl;
+    return decimal;
 }
 
+int ASCII_DEC(unsigned char entrada, int bandera)
+{
+    size_t i;
+    int m[8], conv[8], top, bottom, decimal=0;
+    //int conv[8];
+
+    for(i=0;i<8;i++)
+    {
+        m[i]=entrada%2;
+        entrada = entrada/2;
+    }
+
+    for(bottom=0,top =7; bottom<8; bottom++,top--)
+    {
+        conv[bottom] = m[top];
+    }
+
+    for (i=bandera;i<=7;i++)
+    {
+        switch (i)
+        {
+        case 0:
+            if (conv[i]==1)
+            {
+                decimal=decimal+128;
+            }
+        break;
+
+        case 1:
+            if (conv[i]==1)
+            {
+                decimal=decimal+64;
+            }
+        break;
+
+        case 2:
+            if (conv[i]==1)
+            {
+                decimal=decimal+32;
+            }
+        break;
+
+        case 3:
+            if (conv[i]==1)
+            {
+                decimal=decimal+16;
+            }
+        break;
+
+        case 4:
+            if (conv[i]==1)
+            {
+                decimal=decimal+8;
+            }
+        break;
+
+        case 5:
+            if (conv[i]==1)
+            {
+                decimal=decimal+4;
+            }
+        break;
+
+        case 6:
+            if (conv[i]==1)
+            {
+                decimal=decimal+2;
+            }
+        break;
+
+        case 7:
+            if (conv[i]==1)
+            {
+                decimal=decimal+1;
+            }
+        break;
+        }
+    }
+    //cout << dec << decimal << endl;
+    return decimal;
+}
+
+int ASCII_DEC(unsigned char prt1, unsigned char prt2)
+{
+    size_t i;
+    int m[8], conv[16], top, bottom, decimal=0;
+
+    for(i=0;i<8;i++)
+    {
+        m[i]=prt1%2;
+        prt1 = prt1/2;
+    }
+
+    for(bottom=0,top =7; bottom<8; bottom++,top--)
+    {
+        conv[bottom] = m[top];
+    }
+
+    for(i=0;i<8;i++)
+    {
+        m[i]=prt2%2;
+        prt2 = prt2/2;
+    }
+
+    for(bottom=0,top =7; bottom<8; bottom++,top--)
+    {
+        conv[bottom+8] = m[top];
+    }
+
+    for (i=0;i<16;i++)
+    {
+        switch (i)
+        {
+        case 0:
+            if (conv[i]==1)
+            {
+                decimal=decimal+32768;
+            }
+        break;
+
+        case 1:
+            if (conv[i]==1)
+            {
+                decimal=decimal+16384;
+            }
+        break;
+
+        case 2:
+            if (conv[i]==1)
+            {
+                decimal=decimal+8192;
+            }
+        break;
+
+        case 3:
+            if (conv[i]==1)
+            {
+                decimal=decimal+4096;
+            }
+        break;
+
+        case 4:
+            if (conv[i]==1)
+            {
+                decimal=decimal+2048;
+            }
+        break;
+
+        case 5:
+            if (conv[i]==1)
+            {
+                decimal=decimal+1024;
+            }
+        break;
+
+        case 6:
+            if (conv[i]==1)
+            {
+                decimal=decimal+512;
+            }
+        break;
+
+        case 7:
+            if (conv[i]==1)
+            {
+                decimal=decimal+256;
+            }
+        break;
+
+        case 8:
+            if (conv[i]==1)
+            {
+                decimal=decimal+128;
+            }
+        break;
+
+        case 9:
+            if (conv[i]==1)
+            {
+                decimal=decimal+64;
+            }
+        break;
+
+        case 10:
+            if (conv[i]==1)
+            {
+                decimal=decimal+32;
+            }
+        break;
+
+        case 11:
+            if (conv[i]==1)
+            {
+                decimal=decimal+16;
+            }
+        break;
+
+        case 12:
+            if (conv[i]==1)
+            {
+                decimal=decimal+8;
+            }
+        break;
+
+        case 13:
+            if (conv[i]==1)
+            {
+                decimal=decimal+4;
+            }
+        break;
+
+        case 14:
+            if (conv[i]==1)
+            {
+                decimal=decimal+2;
+            }
+        break;
+
+        case 15:
+            if (conv[i]==1)
+            {
+                decimal=decimal+1;
+            }
+        break;
+
+        default:break;
+        }
+    }
+    return decimal;
+}
 
 #endif
